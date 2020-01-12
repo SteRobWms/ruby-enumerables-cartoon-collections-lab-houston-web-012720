a2f31d972150db2832c1392592682ed704e18615
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-12 03:11:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves (array)
   dwarves_in_attendance = array.each_with_index
-  dwarves_in_attendance
+  p dwarves_in_attendance
 end
 
 def summon_captain_planet# code an argument here
